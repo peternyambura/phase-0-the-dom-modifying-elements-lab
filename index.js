984d5b77193113fd5c39250f2bba8c0ca14f4b5b
= <peterndirangu0005@gmail.com>
2023-07-19 09:33:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.querySelector('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+document.body.append(newHeader);
